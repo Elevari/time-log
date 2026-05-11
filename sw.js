034d869c46ae83602043a7e27e3891db8aa93dfb
Elevari <36955032+Elevari@users.noreply.github.com>
2026-05-11 10:14:03 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'timelog-v3';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const ASSETS = ['/time-log/', '/time-log/index.html', '/time-log/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
